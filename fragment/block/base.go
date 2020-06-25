a3a3a7c346a48373cf07dd4ab2f280c31c34bd11
anilamilineni <58982496+anilamilineni@users.noreply.github.com>
2020-06-25 10:50:17 +0530

    Update base.go

--- fragment/block/base.go
+++ fragment/block/base.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"html"
 
-	"github.com/SoCloz/goprismic/fragment/link"
-	"github.com/SoCloz/goprismic/fragment/span"
+	"github.com/anilamilineni/goprismic/fragment/link"
+	"github.com/anilamilineni/goprismic/fragment/span"
 )
 
 // Common block properties
